f0048f36bd19673f28c6c75cbc0ace97e2ecaeb4
Adimulla Raju <adimullaraju65@gmail.com>
2025-01-04 11:13:44 +0530

    Add files via upload

---
+++ simplemavenjava/src/main/java/com/sample/maven/simplemavenjava/App.java
@@ -0,0 +1,10 @@
+package com.sample.maven.simplemavenjava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
